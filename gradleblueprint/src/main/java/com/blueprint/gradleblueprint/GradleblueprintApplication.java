4b86419cccb9181093ddc612068e189bc3911878
Karel Muñiz <oricha@gmail.com>
2022-05-09 23:36:49 +0200

    Gradle Blueprint

---
+++ gradleblueprint/src/main/java/com/blueprint/gradleblueprint/GradleblueprintApplication.java
@@ -0,0 +1,13 @@
+package com.blueprint.gradleblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleblueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleblueprintApplication.class, args);
+	}
+
+}
---
+++ gradleblueprint/src/test/java/com/blueprint/gradleblueprint/GradleblueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueprint.gradleblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleblueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
